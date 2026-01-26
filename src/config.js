916206d21934096a36e3acf7f79ed5a9b0557c91
Steven Tok Meng Shan <steventok@Stevens-15-inch.local>
2026-01-27 01:01:02 +0800

    fixes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.RENDER_API_URL || "http://localhost:5050";
+const API_URL = import.meta.env.VITE_RENDER_API_URL || "http://localhost:5050";
 
 export default API_URL;
